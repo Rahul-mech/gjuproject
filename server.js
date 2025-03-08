4b9f42a4757cdd7effd99b568e06f4b0e561492f
Rahul-mech <gurumaincastle1@gmail.com>
2025-03-08 23:07:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,33 +6,39 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-let users = [];
+// Store connected clients
+let clients = [];
 
 wss.on("connection", (ws) => {
     console.log("New client connected!");
-    users.push(ws);  // Add the user to the queue
+    clients.push(ws);
 
-    if (users.length === 2) {
-        const user1 = users.shift(); // Get the first user
-        const user2 = users.shift(); // Get the second user
+    // Once two users are connected, pair them
+    if (clients.length === 2) {
+        const [user1, user2] = clients;
 
-        // Pair the users together by sending each other a signal
-        user1.send(JSON.stringify({ type: "start_call", message: "You are paired with someone!" }));
-        user2.send(JSON.stringify({ type: "start_call", message: "You are paired with someone!" }));
-
-        // Handle the message flow between the two
-        user1.on("message", (message) => {
-            user2.send(message); // Send user1's message to user2
-        });
-        user2.on("message", (message) => {
-            user1.send(message); // Send user2's message to user1
-        });
+        // Send 'pairing' message to both users
+        user1.send(JSON.stringify({ type: "pairing", partner: 2 }));
+        user2.send(JSON.stringify({ type: "pairing", partner: 1 }));
     }
+
+    ws.on("message", (message) => {
+        console.log("Received message:", message);
+        const data = JSON.parse(message);
+
+        if (data.type === "offer" || data.type === "answer" || data.type === "ice") {
+            // Send the message to the other user
+            clients.forEach((client, index) => {
+                if (index !== clients.indexOf(ws)) {
+                    client.send(message);
+                }
+            });
+        }
+    });
 
     ws.on("close", () => {
         console.log("Client disconnected.");
-        // Clean up the user queue on disconnect
-        users = users.filter((user) => user !== ws);
+        clients = clients.filter(client => client !== ws); // Remove client from the list
     });
 });
 
